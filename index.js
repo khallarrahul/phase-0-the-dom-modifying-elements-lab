11fe2e4b5f9309f164469f66943e30f1b0cd102d
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-03-03 16:08:52 -0800

    adds modifying DOM

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const ul = document.getElementsByTagName('main')[0];
+ul.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = "victory";
+
+newHeader.textContent =  'Rahul Khallar is the champion';
